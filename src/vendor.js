5d54963740cffc6414733dbb549bda3802845601
Andrea Briganti <kbytesys@gmail.com>
2020-04-27 02:33:58 +0200

    Update of all packages and a different way to customize bootstrap

--- src/vendor.js
+++ src/vendor.js
@@ -1,3 +1,4 @@
 require('../scss/custom-bootstrap.scss');
 require('../node_modules/jquery/dist/jquery.slim.min');
+require('../node_modules/@popperjs/core/dist/umd/popper.min');
 require('../node_modules/bootstrap/dist/js/bootstrap.bundle.min');
